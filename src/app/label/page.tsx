8f75a17e1721af8e463fb47b03768f09e79be72e
kaneeldias <kaneeldias@hotmail.com>
2026-04-23 18:28:17 +0530

    Remove commented-out domain names from page component data retrieval

--- src/app/label/page.tsx
+++ src/app/label/page.tsx
@@ -5,20 +5,8 @@ import PageLabeller from "@/components/custom/page_labeller";
 
 export default function LabelPage() {
     const {data, loading, error} = usePage([
-        "homelux.lk",
-        "bigdeals.lk",
-        "strong.lk",
-        "mysoftlogic.lk",
-        "ugreen.lk",
-        "buyabans.com",
-        "singhagiri.lk",
-        "pettahkade.lk",
-        "finez.lk",
-        "raesl.lk",
-        "damro.lk",
-        "fireworks.lk",
-        "nanotek.lk",
-        "simplytek.lk",
+        // "geniusmobile.lk",
+        // "dialcom.lk"
     ]);
 
 
